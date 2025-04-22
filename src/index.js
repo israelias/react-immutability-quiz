7c5a7a98db9647d6079a3fa116f871e5dc55824d
J. Elias Sañez <israelias.js@gmail.com>
2025-04-22 16:35:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./styles.css";
 import Button1 from "./Button1";
 import Button2 from "./Button2";
 import Button3 from "./Button3";
+import Button4 from "./Button3";
 
 /**
  * React Hooks Equality Comparison Quiz
@@ -24,9 +25,10 @@ import Button3 from "./Button3";
 const App = () => {
   return (
     <>
-      <Button1 />
+      {/* <Button1 /> */}
       {/* <Button2 /> */}
       {/* <Button3 /> */}
+      <Button4 />
     </>
   );
 };
